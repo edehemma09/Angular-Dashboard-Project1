51b59c4f067ddceabe488fdb02a5eefdd34c963b
edehemma09 <edehemmanuelebubechukwu@gmail.com>
2022-07-29 14:01:28 +0100

    initial commit: Added feature for the dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LoginComponent } from './Components/login/login.component';
+import { PasswordComponent } from './Components/password/password.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { ToggleComponent } from './Components/toggle/toggle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NavbarComponent},
+  {path: 'Home', component: ToggleComponent, pathMatch: 'full'},
+    {path:'login',component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'changepassword', component: PasswordComponent}
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ToggleComponent } from './Components/toggle/toggle.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToggleComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
